c89f35fe921840168e2c4b0e1fe7d5945bcfdfd3
Tom <thomashollis1@gmail.com>
2018-09-19 23:23:45 -0400

    v1.2

--- scripts/Ex1.10.1.R
+++ scripts/Ex1.10.1.R
@@ -1,7 +1,7 @@
 cnt <- 3 #numeric count
 txt <- as.character(cnt) #character count
-while (cnt > -1) { #break condition
-    txt <- c(txt, cnt) #append new character to count
+while (cnt > 0) { #break condition
     cnt = cnt - 1 #decrement
+    txt <- c(txt, cnt) #append new character to count
 }
 (txt <- c(txt, "Lift Off!")) #append final string & print
